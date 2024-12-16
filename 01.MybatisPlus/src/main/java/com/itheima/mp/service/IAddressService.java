e4068403378f71e62410588b8f5cecc0fd6aac57
FengYuchenGuang <ankskfp237@163.com>
2024-12-16 21:30:25 +0800

    2024/12/16

---
+++ 01.MybatisPlus/src/main/java/com/itheima/mp/service/IAddressService.java
@@ -0,0 +1,16 @@
+package com.itheima.mp.service;
+
+import com.itheima.mp.domain.po.Address;
+import com.baomidou.mybatisplus.extension.service.IService;
+
+/**
+ * <p>
+ *  服务类
+ * </p>
+ *
+ * @author hxz
+ * @since 2024-12-16
+ */
+public interface IAddressService extends IService<Address> {
+
+}
